8bec61776c1f486992fbc33d7ceb825197804e66
David Coats <david@getsafetycone.com>
2021-05-17 22:25:30 -0700

    Add incomplete IOrganizationsDirectoryPathProvider service implementation.

---
+++ source/R5T.Costobocia.Default/Code/Services/Implementations/OrganizationsDirectoryPathProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Costobocia.Default
+{
+    public class OrganizationsDirectoryPathProvider : IOrganizationsDirectoryPathProvider
+    {
+        public string GetOrganizationsDirectoryPath()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
